a5959587eb6bb43042bde94dfc20314ed763872f
blacktache <adnanemandili@gmail.com>
2024-08-06 10:46:10 +0100

    first commit - the base of the project-

---
+++ Convert-XML/src/main/java/com/example/Convert_XML/ConvertXmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.Convert_XML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertXmlApplication.class, args);
+	}
+
+}
---
+++ Convert-XML/src/test/java/com/example/Convert_XML/ConvertXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Convert_XML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
